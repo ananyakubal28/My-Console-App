260dfb949518cef467bc7917e65abe0583e1461c
ananyakubal28 <91237049+ananyakubal28@users.noreply.github.com>
2022-01-12 17:26:46 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+var first_name = "Ranbir ";
+var last_name = "Singh";
+var full_name = first_name.concat(last_name);
+console.log(full_name);
+var a = 10;
+var b = 5;
+var add = a+b;
+console.log(add);
+var sub = a-b;
+console.log(sub);
+var mul = a*b;
+console.log(mul);
+var div = a/b;
+console.log(div);
